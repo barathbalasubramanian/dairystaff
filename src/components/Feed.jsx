0a6aa8c13c87e193024b7146bee863796698bcb3
barathbalasubramanian <105224717+barathbalasubramanian@users.noreply.github.com>
2024-10-12 14:59:35 +0530

    UI Changes ..

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -99,94 +99,114 @@ const Feed = () => {
       {ShowOrderCon && (
         <div className="formerservice-feed1">
           <div className="formerservice-feed-head1">
-            <h2>Confirmation</h2>
+            <h2 className="text-center w-full text-xl font-medium">Confirmation</h2>
             <FaTimes onClick={() => handleCon()} />
           </div>
-          <div className="formerservice-feed-table1">
+          <div className="formerservice-feed-table1 pb-10">
             <img src={pic} alt="" />
             <h3>Order has been Successfully Placed</h3>
           </div>
         </div>
       )}
-      <div className="top-bar">
-        <label>
-          <input
-            type="radio"
-            value="feed"
-            checked={selectedCategory === "feed"}
-            onChange={() => setSelectedCategory("feed")}
-          />
+
+      <div className="flex items-center mt-4 py-10">
+        <button
+          onClick={() => setSelectedCategory("feed")}
+          className={`flex-1 text-center py-2 rounded-l-lg transition-colors 
+            ${selectedCategory === "feed" ? "bg-green-500 text-white" : "bg-gray-200 text-gray-700 hover:bg-blue-100"}`}
+        >
           Feed
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="supplementary"
-            checked={selectedCategory === "supplementary"}
-            onChange={() => setSelectedCategory("supplementary")}
-          />
-          Supplementary food
-        </label>
+        </button>
+        <button
+          onClick={() => setSelectedCategory("supplementary")}
+          className={`flex-1 w-[28em]  text-center py-2 rounded-r-lg transition-colors 
+            ${selectedCategory === "supplementary" ? "bg-blue-600 text-white" : "bg-gray-200 text-gray-700 hover:bg-blue-100"}`}
+        >
+          Supplementary Food
+        </button>
       </div>
-      <div className="content">
-        <div className="product-list">
+      <div className="flex w-[80%] justify-between max-lg:flex-col max-lg:gap-10 py-10">
+        <div className="w-[45%] max-lg:w-[80%] m-auto">
           {products.map((product) => (
-            <div className="product" key={product.id}>
-              <div className="product-details">
-                <h4> {product.Manufacturer}   {product.name}</h4>
-                <p>INR {product.price}</p>
-              </div>
-              <div className="quantity-control">
-                <button onClick={() => updateQuantity(product, -1)}>-</button>
-                <div className="quantity-control-qty">
-                  <h3>{getProductQuantity(product.id)}</h3>
+            <div className="flex items-center justify-between bg-white p-4 rounded-lg shadow-md mb-4">
+              <div className="flex items-center text-start">
+                <div className="ml-4">
+                  <h4 className="text-lg font-medium capitalize text-gray-700">{product.Manufacturer} {product.name}</h4>
+                  <div className="text-md text-gray-500">₹ {product.price}</div>
                 </div>
-                <button onClick={() => addToCart(product)}>+</button>
+              </div>        
+              <div className="flex items-center">
+                <button 
+                  onClick={() => updateQuantity(product, -1)} 
+                  className="bg-gray-300 text-gray-700 w-8 h-8 flex items-center justify-center rounded-l hover:bg-gray-400 transition-colors">
+                  -
+                </button>
+                <div className="bg-gray-100 w-12 text-center flex items-center justify-center">
+                  <h3 className="text-lg">{getProductQuantity(product.id)}</h3>
+                </div>
+                <button 
+                  onClick={() => addToCart(product)} 
+                  className="bg-gray-300 text-gray-700 w-8 h-8 flex items-center justify-center rounded-r hover:bg-gray-400 transition-colors">
+                  +
+                </button>
               </div>
             </div>
           ))}
         </div>
-        <div className="cart">
-          <div className="cart-title">
-            <svg
-              width="30"
-              height="29"
-              viewBox="0 0 30 29"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11.7498 8.83331V5.99998H20.2498V8.83331H11.7498ZM8.9165 28.6666C8.13734 28.6666 7.47032 28.3892 6.91546 27.8344C6.3606 27.2795 6.08317 26.6125 6.08317 25.8333C6.08317 25.0541 6.3606 24.3871 6.91546 23.8323C7.47032 23.2774 8.13734 23 8.9165 23C9.69567 23 10.3627 23.2774 10.9175 23.8323C11.4724 24.3871 11.7498 25.0541 11.7498 25.8333C11.7498 26.6125 11.4724 27.2795 10.9175 27.8344C10.3627 28.3892 9.69567 28.6666 8.9165 28.6666ZM23.0832 28.6666C22.304 28.6666 21.637 28.3892 21.0821 27.8344C20.5273 27.2795 20.2498 26.6125 20.2498 25.8333C20.2498 25.0541 20.5273 24.3871 21.0821 23.8323C21.637 23.2774 22.304 23 23.0832 23C23.8623 23 24.5294 23.2774 25.0842 23.8323C25.6391 24.3871 25.9165 25.0541 25.9165 25.8333C25.9165 26.6125 25.6391 27.2795 25.0842 27.8344C24.5294 28.3892 23.8623 28.6666 23.0832 28.6666ZM0.416504 3.16665V0.333313H5.05609L11.0769 13.0833H20.9936L26.5186 3.16665H29.7415L23.5082 14.4291C23.2484 14.9014 22.9002 15.2673 22.4634 15.5271C22.0266 15.7868 21.5484 15.9166 21.029 15.9166H10.4748L8.9165 18.75H25.9165V21.5833H8.9165C7.854 21.5833 7.04532 21.1229 6.49046 20.2021C5.9356 19.2812 5.91789 18.3486 6.43734 17.4041L8.34984 13.9333L3.24984 3.16665H0.416504Z"
-                fill="#1C1B1F"
-              />
-            </svg>
 
-            <h3>Cart</h3>
+
+        <div className="w-[45%] m-auto max-lg:w-[80%] rounded-md bg-[#e0f7fa] p-4 py-10 flex items-center flex-col shadow-sm justify-between">
+
+          <div className="cart-title">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-76.41q-33 0-56.5-23.5t-23.5-56.5q0-33 23.5-56.5t56.5-23.5q33 0 56.5 23.5t23.5 56.5q0 33-23.5 56.5T280-76.41Zm400 0q-33 0-56.5-23.5t-23.5-56.5q0-33 23.5-56.5t56.5-23.5q33 0 56.5 23.5t23.5 56.5q0 33-23.5 56.5T680-76.41Zm-428.02-640 90.5 189h279.04l103.78-189H251.98Zm-41.83-87.18h580.92q27.21 0 41.41 24.09 14.19 24.09 1.04 48.91l-136.5 246.68q-11.48 20.71-30.8 32.31-19.33 11.6-42.31 11.6H328.07l-41.85 76.41h477.37v87.18H280q-47.63 0-71.59-41.42-23.95-41.41-1.76-82.08L259.93-496 117.85-796.41H36.41v-87.18h135.74l38 80Zm132.33 276.18h279.04-279.04Z"/></svg>            
+            <div className="font-medium text-lg pl-3">Cart</div>
           </div>
-          {cart && cart.map((item) => (
-            <div className="cart-item" key={item.id}>
-              <div className="cart-item-content">
-                <p>{item.Manufacturer}    {item.name}</p>
-                <div className="quantity-control">
-                  <button onClick={() => updateQuantity(item, -1)}>-</button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => updateQuantity(item, 1)}>+</button>
+
+          <div className="space-y-2 py-6 w-[90%]"> 
+            {cart && cart.map((item) => (
+              <div className="min-w-full flex items-center justify-between bg-white p-4 rounded-lg shadow-md" key={item.id}>
+
+                <div className="flex items-center w-full">
+                  <div className="mr-4 text-start">
+                    <p className="text-lg font-medium">{item.Manufacturer} {item.name}</p>
+                    <div className="text-md text-gray-500">₹ {item.price}</div>
+                  </div>
+                </div>
+
+                <div className="flex items-center space-x-2">
+                  <button 
+                    onClick={() => updateQuantity(item, -1)} 
+                    className={`bg-gray-300 text-gray-700 w-8 h-8 rounded-full flex items-center justify-center 
+                      ${item.quantity <= 0 ? 'opacity-50 cursor-not-allowed' : 'hover:bg-gray-400 transition-colors'}`}
+                    disabled={item.quantity <= 0}
+                  >
+                    -
+                  </button>
+                  <span className="text-lg font-semibold">{item.quantity}</span>
+                  <button 
+                    onClick={() => updateQuantity(item, 1)} 
+                    className="bg-gray-300 text-gray-700 w-8 h-8 rounded-full flex items-center justify-center hover:bg-gray-400 transition-colors">
+                    +
+                  </button>
+                </div>
+
+                <div className="pl-2" onClick={() => updateQuantity(item, -item.quantity)} >
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M280.37-121.87q-35.72 0-60.86-25.14t-25.14-60.86v-506.5h-44.5v-86H356.5v-43.5h247.52v43.5h207.11v86h-44.5v506.5q0 35.72-25.14 60.86t-60.86 25.14H280.37Zm400.26-592.5H280.37v506.5h400.26v-506.5ZM360.7-283.74h80.5v-355h-80.5v355Zm159.1 0h80.5v-355h-80.5v355ZM280.37-714.37v506.5-506.5Z"/></svg>
                 </div>
               </div>
-              <div>INR {item.price}</div>
-              <FaTimes
-                className="remove-item"
-                onClick={() => updateQuantity(item, -item.quantity)}
-              />
-            </div>
-          ))}
-          <div className="check-but">
-            <button className="cancel" onClick={cancelCart}>Cancel</button>
-            <button className="place-order" onClick={handleSubmit}>
-              Place Order
-            </button>
+            ))}
           </div>
-          <div className="total">Grand Total: INR {getTotal()}</div>
+
+          <div className="w-full flex flex-col items-center">
+            <div className="py-4 flex items-center w-[90%] justify-between gap-6">
+              <button className="px-4 py-2 min-w-fit text-[#06AD9D] rounded-sm border border-[#06AD9D]" onClick={cancelCart}>Cancel</button>
+              <button className="px-4 py-2 min-w-fit bg-[#06AD9D] text-white rounded-sm" onClick={handleSubmit}>
+                Place Order
+              </button>
+            </div>
+
+            <div className="mt-2 w-[90%] text-center px-10 py-2 bg-[#4695B8] text-white rounded-md" >Grand Total : ₹{getTotal()}</div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Sp.jsx
+++ src/components/Sp.jsx
@@ -21,71 +21,157 @@ const Sp = () => {
 
   const Card = (item, index) => {
     return (
-      <div className="sp-card" id={index}>
-        <div className="sp-card-header">
-          <p className="sp-id">{item.Doctor_id}</p>
-          <p className="sp-name">{item.Name}</p>
-          <span className="sp-status">
-            <span className="sp-dot"></span> Available
+      <div className="bg-white shadow-lg rounded-lg p-4 m-4 w-[20em]" id={index}>
+
+        <div className="flex justify-between items-center border-b pb-2 mb-2">
+          <div>
+            <p className="text-xl font-medium capitalize text-gray-800">{item.Name}</p>
+            <p className="text-gray-500 text-sm">ID: {item.Doctor_id}</p>
+          </div>
+          <span className="flex items-center">
+            <span className="h-2 w-2 bg-green-500 rounded-full mr-2"></span> 
+            <span className="text-sm text-green-500">Available</span>
           </span>
         </div>
-        <div className="sp-card-body">
-          <button className="sp-btn sp-cluster">
-            Cluster: {item.ClusterName}
-          </button>
-          {item.Type === 1 ? (
-            <button className="sp-btn sp-department">VETRINARY</button>
-          ) : (
-            <button className="sp-btn sp-department">AI</button>
-          )}
 
-          <div className="sp-contact-info">
-            <p className="sp-email">📧 {item.Email}</p>
-            <p className="sp-phone">📞 {item.Phno}</p>
+        <div className="space-y-2 space-x-1">
+          <button className="bg-black capitalize text-white px-3 py-1 rounded transition">
+            {item.ClusterName}
+          </button>
+          <button className={`bg-green-500 text-white px-3 py-1 rounded transition`}>
+            {item.Type === 1 ? 'VETERINARY' : 'AI'}
+          </button>
+
+          <div className="text-gray-600 pt-2">
+            <p className="flex items-center text-sm gap-1">
+              <span role="img" aria-label="email">📧</span> {item.Email}
+            </p>
+            <p className="flex items-center text-sm gap-1">
+              <span role="img" aria-label="phone">📞</span> {item.Phno}
+            </p>
           </div>
         </div>
-        <button className="sp-connect-btn">Connect 📞</button>
+
+        <button className="bg-[#00ccb1] text-white mt-4 w-full py-2 rounded transition">
+          Connect 📞
+        </button>
+
       </div>
+
+      // <div className="sp-card" id={index}>
+      //   <div className="sp-card-header">
+      //     <p className="sp-id">{item.Doctor_id}</p>
+      //     <p className="sp-name">{item.Name}</p>
+      //     <span className="sp-status">
+      //       <span className="sp-dot"></span> Available
+      //     </span>
+      //   </div>
+      //   <div className="sp-card-body">
+      //     <button className="sp-btn sp-cluster">
+      //       Cluster: {item.ClusterName}
+      //     </button>
+      //     {item.Type === 1 ? (
+      //       <button className="sp-btn sp-department">VETRINARY</button>
+      //     ) : (
+      //       <button className="sp-btn sp-department">AI</button>
+      //     )}
+
+      //     <div className="sp-contact-info">
+      //       <p className="sp-email">📧 {item.Email}</p>
+      //       <p className="sp-phone">📞 {item.Phno}</p>
+      //     </div>
+      //   </div>
+      //   <button className="sp-connect-btn">Connect 📞</button>
+      // </div>
     );
   };
+
   const UnCard = (item, index) => {
     return (
-      <div className="spu-card" id={index}>
-        <div className="spu-card-header">
-          <div className="spu-card-name">
-            <p className="spu-id">{item.Doctor_id}</p>
-            <p className="spu-name">{item.Name}</p>
-          </div>
+      <div className="bg-white shadow-lg rounded-lg p-5 m-4 w-[20em]" id={index}>
 
-          <span className="spu-status">
-            Available On
-            <span className="spu-date">{dateconvert(item.Availableon)}</span>
+        <div className="flex justify-between items-center border-b pb-3 mb-3">
+          <div>
+            <p className="text-lg font-medium capitalize text-gray-800">{item.Name}</p>
+            <p className="text-gray-500">ID: {item.Doctor_id}</p>
+          </div>
+          <span className="text-sm font-medium text-red-600 w-[42%]">
+            Available On: 
+            <span className="font-medium text-red-600 ml-1">{dateconvert(item.Availableon)}</span>
           </span>
         </div>
-        <div className="spu-card-body">
-          <button className="spu-btn spu-cluster">
+
+        <div className="space-y-3 space-x-2">
+          <button className="bg-black text-white px-3 py-1 rounded transition">
             Cluster: {item.ClusterName}
           </button>
-          {item.Type === 1 ? (
-            <button className="sp-btn spu-department">VETRINARY</button>
-          ) : (
-            <button className="sp-btn spu-department">AI</button>
-          )}
-          <div className="spu-contact-info">
-            <p className="spu-email">📧 {item.Email}</p>
-            <p className="spu-phone">📞 {item.Phno}</p>
+          <button className={`bg-green-500 text-white px-3 py-1 rounded transition`}>
+            {item.Type === 1 ? 'VETERINARY' : 'AI'}
+          </button>
+          
+          {/* Contact Information */}
+          <div className="text-gray-600 pt-2">
+            <p className="flex items-center text-[14px]">
+              <span role="img" aria-label="email" className="mr-1">📧</span>
+              {item.Email}
+            </p>
+            <p className="flex items-center text-[14px]">
+              <span role="img" aria-label="phone" className="mr-1">📞</span>
+              {item.Phno}
+            </p>
           </div>
-          <div className="spu-comments-section">
-            <label htmlFor="spu-comments">Comment:</label>
+
+          {/* Comments Section */}
+          <div className="space-y-1 pt-4">
+            <label htmlFor="comments" className="text-gray-700 font-medium">Comment:</label>
             <textarea
               id="comments"
               rows="4"
-              placeholder="Some thing Else Comments..."
+              placeholder="Some other comments..."
               value={item.Reason}
+              className="w-full border border-gray-300 rounded p-2 focus:outline-none"
             ></textarea>
           </div>
         </div>
+
       </div>
+
+      // <div className="spu-card" id={index}>
+      //   <div className="spu-card-header">
+      //     <div className="spu-card-name">
+      //       <p className="spu-id">{item.Doctor_id}</p>
+      //       <p className="spu-name">{item.Name}</p>
+      //     </div>
+
+      //     <span className="spu-status">
+      //       Available On
+      //       <span className="spu-date">{dateconvert(item.Availableon)}</span>
+      //     </span>
+      //   </div>
+      //   <div className="spu-card-body">
+      //     <button className="spu-btn spu-cluster">
+      //       Cluster: {item.ClusterName}
+      //     </button>
+      //     {item.Type === 1 ? (
+      //       <button className="sp-btn spu-department">VETRINARY</button>
+      //     ) : (
+      //       <button className="sp-btn spu-department">AI</button>
+      //     )}
+      //     <div className="spu-contact-info">
+      //       <p className="spu-email">📧 {item.Email}</p>
+      //       <p className="spu-phone">📞 {item.Phno}</p>
+      //     </div>
+      //     <div className="spu-comments-section">
+      //       <label htmlFor="spu-comments">Comment:</label>
+      //       <textarea
+      //         id="comments"
+      //         rows="4"
+      //         placeholder="Some thing Else Comments..."
+      //         value={item.Reason}
+      //       ></textarea>
+      //     </div>
+      //   </div>
+      // </div>
     );
   };
   
@@ -95,24 +181,28 @@ const Sp = () => {
       <div className="sp-container">
         <Search />
         <div className="sp-box">
-          <div className="sp-tabs">
+          <div className="flex space-x-4 w-[80%] items-center justify-center m-auto">
             <div className="sp-inputgroup">
               <button
                 onClick={() => setTab("Availabe")}
-                className={tab === "Availabe" ? "active" : ""}
+                className={`py-2 px-4 rounded-lg transition-colors duration-200 
+                  ${tab === "Availabe" ? "bg-green-500 text-white" : "bg-gray-200 text-gray-800 hover:bg-gray-300"}`}
               >
                 Available
               </button>
             </div>
+            
             <div className="sp-inputgroup">
               <button
                 onClick={() => setTab("UnAvailable")}
-                className={tab === "UnAvailable" ? "active" : ""}
+                className={`py-2 px-4 rounded-lg transition-colors duration-200 
+                  ${tab === "UnAvailable" ? "bg-red-500 text-white" : "bg-gray-200 text-gray-800 hover:bg-gray-300"}`}
               >
-                UnAvailable
+                Unavailable
               </button>
             </div>
           </div>
+
           {tab === "Availabe" && (
             <>
               <div className="sp-cards">
